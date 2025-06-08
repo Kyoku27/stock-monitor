77d8c91ffdd281a6d5293b2fb50f7688ba1d9637
kyoku <119828515+Kyoku27@users.noreply.github.com>
2025-06-08 12:59:45 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -10,3 +10,6 @@ def rakuten_stock():
         return jsonify({"error": "Please provide SKU(s) via ?sku=SKU1&sku=SKU2"}), 400
     data = get_rakuten_inventory(sku_list)
     return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
