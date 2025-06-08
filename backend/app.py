bc0365ff8c26882b6a4f56e900d6e823220bea6e
kyoku <119828515+Kyoku27@users.noreply.github.com>
2025-06-08 19:25:43 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from rakuten_api import get_rakuten_inventory
 
@@ -17,3 +18,4 @@ def rakuten_stock():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
